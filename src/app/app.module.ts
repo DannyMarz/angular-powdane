a138143fa620e8ed3911270c877f1f2ff6717e1f
DannyMarz <dannyrajesh39@gmail.com>
2020-08-18 17:21:48 +0530

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import {Routes, RouterModule} from '@angular/router'
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {ProfileComponent} from './profile/profile.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+    topics=['Angular','vue','React'];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
