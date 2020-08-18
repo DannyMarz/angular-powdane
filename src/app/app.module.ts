b4ea88427e8c12e9c032d48b9004eb77c204d763
DannyMarz <dannyrajesh39@gmail.com>
2020-08-18 16:29:52 +0530

    4 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,  ],
+  imports:      [ BrowserModule, FormsModule, RouterModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
